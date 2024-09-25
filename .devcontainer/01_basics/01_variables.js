a211bd4de799f55fe2a83f9921f9a86ad429c16b
anillohar14 <ashwin14lohar@gmail.com>
2024-09-25 17:06:35 +0000

    learn 01_variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 123
+let accountEmail = "neew@gmail.com"
+let accountPassword = "9820440083"
+accountCity = "Mumbai";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
