51eb37dc6073df8368bd1ad0270a4e5c6750e7fa
anillohar14 <ashwin14lohar@gmail.com>
2024-09-26 17:48:16 +0000

    learned strings

---
+++ .devcontainer/01_basics/02_srings.js
@@ -0,0 +1,17 @@
+console.log("anil lohar");
+
+let play = "anil-lohar";
+console.log( "triming  : " + console.log(play.trim()));
+console.log( "Finding By Index :  " +play.charAt(2));
+console.log( "Finding By character :  " +play.indexOf('o'));
+console.log( "Slicing :  " +play.slice(0 , 4));
+console.log( "Finding By Index :  " +play.charAt(2));
+console.log( "UpperCase : " +play.toUpperCase());
+console.log( "LowerCase :  " +play.toLowerCase());
+console.log();
+
+const url = "https://anil_lohar.com/menu";
+console.log( "replacing : " +url.replace('_','@'));
+console.log( "Finding : " +url.includes('lohar'));
+console.log( "replacing : " +url.replace('_','@'));
+console.log( "spliting : " +play.split('-'));
