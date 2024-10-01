90bbc68826655b6731ecff15f2dc9a137f34ae48
anillohar14 <ashwin14lohar@gmail.com>
2024-10-01 18:01:04 +0000

    learned about objects in JS

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -3,4 +3,74 @@ const facebook = {}
     facebook.userName = "ashwin lohar",
     facebook.loggedIn = true
 
-    console.log(facebook.loggedIn);
+    // console.log(facebook);
+    // console.log(facebook.loggedIn);
+
+    // now below code is about nested objects
+
+    const instagram = {
+       user : "ashwin lohar",
+       loggedIn : false,
+       gmail : "ashwin14lohar@gmail.com",
+       location : {
+            state : {
+                state : "maharashtra",
+                city : {
+                    city : "Mumbai"
+                }
+            }
+       }
+
+    }
+
+    // console.log(instagram.location.state.city.city);
+
+
+    //  learn about to merge multiple objects
+    
+    const obj1 ={1 : "a", 2 :"b"}
+    const obj2 ={3 : "a", 4 :"b"}
+    const obj3 ={5 : "a", 6 :"b"}
+
+    // there are many ways to merge multipal objects
+
+    // type 1 
+    const obj4 = {obj1, obj2} // this method create a new object and store in like nested object
+    // console.log(`method no. 1 : `,console.log(obj4));
+    // console.log();
+
+    // type 2
+    const obj5 = Object.assign({}, obj1, obj2, obj3);
+    // console.log(`method no. 2 : ${console.log(obj5)}`); // this method is good but not that much prefered in industry
+    // console.log();
+
+    // type 3
+    const obj6 = {...obj1, ...obj2, ...obj3};
+    // console.log(`method no. 3 : ${console.log(obj5)}`); // this method is most prefered in industry
+
+
+    // now object can be stored in array as well 
+
+    const user = [
+        {
+            id : 1,
+            user_ID : "324ffkwfhr239"
+        },
+        {
+            id : 1,
+            user_ID : "324ffkwfhr239"
+        },
+        {
+            id : 1,
+            user_ID : "324ffkwfhr239"
+        }
+
+    ]
+
+    // console.log(user[1]);
+
+    // now finding values, keys and entries in oblects by using its functions
+
+    console.log(`Printing the values of facebook object : ${Object.values(facebook)}`);
+    console.log(`Printing the keys of facebook object : ${Object.keys(facebook)}`);
+    console.log(`Printing the entries of facebook object : ${Object.entries(facebook)}`);
