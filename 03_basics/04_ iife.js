b6fe7530045cc8541937d0c5a17cabbfed15d9be
anillohar14 <ashwin14lohar@gmail.com>
2024-10-04 18:19:45 +0000

    learned about IIFE function.

---
+++ 03_basics/04_ iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// Below code is an named iife
+(function callme () {
+    console.log("ANIL")
+}) ();
+
+// Below code is an normal iife
+
+((name) => {
+    console.log(name)
+})("anil")
